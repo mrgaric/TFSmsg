fca07067a922d7d4ecb247a5444678ca1fcb1fa9
mrgaric <mr.igordubrovin@gmail.com>
2017-03-31 15:24:41 +0300

    del login error

--- app/src/main/java/com/igordubrovin/tfsmsg/activities/LoginActivity.java
+++ app/src/main/java/com/igordubrovin/tfsmsg/activities/LoginActivity.java
@@ -3,9 +3,6 @@ package com.igordubrovin.tfsmsg.activities;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.BaseTransientBottomBar;
-import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -38,20 +35,29 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void startNextScreen() {
-        if (password.getText().toString().equals("admin")) {
-            Intent intent = new Intent(this, NavigationActivity.class);
-            startActivity(intent);
+
+        SharedPreferences sPref = getSharedPreferences(ProjectConstants.PREFERENCES_LOGIN_FILE_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sPref.edit();
+        editor.putBoolean(ProjectConstants.PREFERENCES_STATE_LOGIN, ProjectConstants.USER_LOGGED);
+        editor.putString(ProjectConstants.USERS_LOGIN, login.getText().toString());
+        editor.apply();
+        Intent intent = new Intent(this, NavigationActivity.class);
+        startActivity(intent);
+
+        /*if (password.getText().toString().equals("admin")) {
             SharedPreferences sPref = getSharedPreferences(ProjectConstants.PREFERENCES_LOGIN_FILE_NAME, MODE_PRIVATE);
             SharedPreferences.Editor editor = sPref.edit();
             editor.putBoolean(ProjectConstants.PREFERENCES_STATE_LOGIN, ProjectConstants.USER_LOGGED);
             editor.putString(ProjectConstants.USERS_LOGIN, login.getText().toString());
             editor.apply();
+            Intent intent = new Intent(this, NavigationActivity.class);
+            startActivity(intent);
         }
         else {
             Snackbar snackbar = Snackbar.make(findViewById(R.id.root_login_view), "Login Error", BaseTransientBottomBar.LENGTH_SHORT);
             snackbar.getView().setBackgroundColor(ContextCompat.getColor(getApplicationContext(), android.R.color.holo_red_dark));
             snackbar.show();
-        }
+        }*/
     }
 }
 
