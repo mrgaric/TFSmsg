d83de5eeb6ab565351f7458835a49a06342950c8
mrgaric <mr.igordubrovin@gmail.com>
2017-04-19 00:42:35 +0400

    upd animation

--- app/src/main/java/com/igordubrovin/tfsmsg/adapters/ImageAdapter.java
+++ app/src/main/java/com/igordubrovin/tfsmsg/adapters/ImageAdapter.java
@@ -1,11 +1,15 @@
 package com.igordubrovin.tfsmsg.adapters;
 
 import android.content.Context;
-import android.graphics.drawable.Animatable;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.drawable.Animatable2;
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -41,27 +45,63 @@ public class ImageAdapter extends BaseAdapter {
         ImageView imageView;
 
         if (convertView == null) {
+            Point size = new Point();
+            WindowManager windowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+            windowManager.getDefaultDisplay().getSize(size);
+            int width = size.x/2;
+            int height = size.y/3;
             imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(170, 170));
+            imageView.setLayoutParams(new GridView.LayoutParams(width, height));
+            imageView.setBackgroundColor(Color.RED);
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageView.setImageResource(mThumbIds[position]);
+            /*final Drawable drawable = imageView.getDrawable();*/
+            /*final AnimatedVectorDrawableCompat animatedVector = AnimatedVectorDrawableCompat.create(mContext, mThumbIds[position]);
+            imageView.setImageDrawable(animatedVector);
+            if (animatedVector != null) {
+                animatedVector.registerAnimationCallback(new Animatable2Compat.AnimationCallback() {
+                    @Override
+                    public void onAnimationEnd(Drawable drawable) {
+                        super.onAnimationEnd(drawable);
+                        animatedVector.start();
+                    }
+                });
+                animatedVector.start();*/
+                final AnimatedVectorDrawable avd;
+                final Drawable drawable = imageView.getDrawable();
+                avd = (AnimatedVectorDrawable) drawable;
 
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    avd.registerAnimationCallback(new Animatable2.AnimationCallback() {
+                        @Override
+                        public void onAnimationEnd(Drawable drawable) {
+                            super.onAnimationEnd(drawable);
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                                avd.start();
+                            }
+                        }
+                    });
+                }
+                ((AnimatedVectorDrawable) drawable).start();
+            }
+
 
         } else {
             imageView = (ImageView) convertView;
         }
-        imageView.setImageResource(mThumbIds[position]);
-        final Drawable drawable = imageView.getDrawable();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            ((Animatable) drawable).start();
-        }
+
+
+
         return imageView;
     }
 
     // Keep all Images in array
     public Integer[] mThumbIds = {
             //here you can place the image
-            R.drawable.avd_vector_anim_cancel, R.drawable.avd_vector_anim_emoji,
-            R.drawable.avd_vector_anim_keyboard, R.drawable.avd_vector_anim_send
+           /* R.drawable.avd_vector_anim_cancel, R.drawable.avd_vector_anim_emoji,
+            R.drawable.avd_vector_anim_keyboard, R.drawable.avd_vector_anim_send,
+            R.drawable.avd_vector_mess, R.drawable.avd_vector_anim_chat*/
+            R.drawable.avd_vector_anim_cancel
     };
-
 }
--- app/src/main/java/com/igordubrovin/tfsmsg/loaders/MessageLoader.java
+++ app/src/main/java/com/igordubrovin/tfsmsg/loaders/MessageLoader.java
@@ -36,7 +36,67 @@ public class MessageLoader extends AsyncTaskLoader<MessageItem> {
         return incomingMessageItem;
     }
 
+    @Override
+    protected void onForceLoad() {
+        super.onForceLoad();
+    }
+
+    @Override
+    protected void onStopLoading() {
+        super.onStopLoading();
+    }
+
+    @Override
+    protected void onAbandon() {
+        super.onAbandon();
+    }
+
+    @Override
+    public void deliverResult(MessageItem data) {
+        super.deliverResult(data);
+    }
+
+    @Override
+    public void deliverCancellation() {
+        super.deliverCancellation();
+    }
+
+    @Override
+    public void onContentChanged() {
+        super.onContentChanged();
+    }
+
+    @Override
+    public void unregisterListener(OnLoadCompleteListener<MessageItem> listener) {
+        super.unregisterListener(listener);
+    }
+
+    @Override
+    public void unregisterOnLoadCanceledListener(OnLoadCanceledListener<MessageItem> listener) {
+        super.unregisterOnLoadCanceledListener(listener);
+    }
+
     private IncomingMessageItem createIncomingMessageItem(String sender, String message){
         return new IncomingMessageItem(sender, message);
+    }
+
+    @Override
+    public void registerOnLoadCanceledListener(OnLoadCanceledListener<MessageItem> listener) {
+        super.registerOnLoadCanceledListener(listener);
+    }
+
+    @Override
+    public void registerListener(int id, OnLoadCompleteListener<MessageItem> listener) {
+        super.registerListener(id, listener);
+    }
+
+    @Override
+    public boolean takeContentChanged() {
+        return super.takeContentChanged();
+    }
+
+    @Override
+    public void rollbackContentChanged() {
+        super.rollbackContentChanged();
     }
 }
