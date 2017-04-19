b2e219571e658ddc6b2d2ccec6614140753547b8
mrgaric <mr.igordubrovin@gmail.com>
2017-04-19 18:47:42 +0300

    infinity animation splash activity

--- app/src/main/java/com/igordubrovin/tfsmsg/activities/SplashActivity.java
+++ app/src/main/java/com/igordubrovin/tfsmsg/activities/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.igordubrovin.tfsmsg.activities;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,10 +11,12 @@ import android.widget.GridView;
 
 import com.igordubrovin.tfsmsg.R;
 import com.igordubrovin.tfsmsg.adapters.ImageAdapter;
+import com.igordubrovin.tfsmsg.utils.ProjectConstants;
 
 public class SplashActivity extends AppCompatActivity {
 
-
+    GridView gridview;
+    ImageAdapter imageAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,25 +25,16 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        /*final ImageView ivSend = (ImageView) findViewById(R.id.anim_iv_send);
-        final ImageView ivKeyb = (ImageView) findViewById(R.id.anim_iv_keyboard);
-        final ImageView ivEmoj = (ImageView) findViewById(R.id.anim_iv_emoj);
-        final ImageView ivCancel = (ImageView) findViewById(R.id.anim_iv_cancel);
-        final Drawable drawableSend = ivSend.getDrawable();
-        final Drawable drawableKeyb = ivKeyb.getDrawable();
-        final Drawable drawableEmoj = ivEmoj.getDrawable();
-        final Drawable drawableCancel = ivCancel.getDrawable();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            ((Animatable) drawableSend).start();
-            ((Animatable) drawableKeyb).start();
-            ((Animatable) drawableEmoj).start();
-            ((Animatable) drawableCancel).start();
-        }*/
-
-        GridView gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setAdapter(new ImageAdapter(this));
-
+        imageAdapter = new ImageAdapter(this);
+        gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(imageAdapter);
         new Loading().execute();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        imageAdapter.interruptThreads();
     }
 
     public class Loading extends AsyncTask<Void, Void, Void> {
@@ -48,7 +43,7 @@ public class SplashActivity extends AppCompatActivity {
         protected Void doInBackground(Void... params) {
 
             try {
-                Thread.sleep(10000);
+                Thread.sleep(5000);
             }catch (InterruptedException e){
                 e.printStackTrace();
             }
@@ -57,14 +52,14 @@ public class SplashActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Void aVoid) {
-            /*super.onPostExecute(aVoid);
+            super.onPostExecute(aVoid);
             Intent intent;
             SharedPreferences sPref = getSharedPreferences(ProjectConstants.PREFERENCES_LOGIN_FILE_NAME, MODE_PRIVATE);
             if (sPref.getBoolean(ProjectConstants.PREFERENCES_STATE_LOGIN, ProjectConstants.USER_NOT_LOGGED))
                 intent = new Intent(getApplicationContext(), NavigationActivity.class);
             else
                 intent = new Intent(getApplicationContext(), LoginActivity.class);
-            startActivity(intent);*/
+            startActivity(intent);
         }
     }
 }
--- app/src/main/java/com/igordubrovin/tfsmsg/adapters/ImageAdapter.java
+++ app/src/main/java/com/igordubrovin/tfsmsg/adapters/ImageAdapter.java
@@ -6,6 +6,7 @@ import android.graphics.Point;
 import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
@@ -15,12 +16,18 @@ import android.widget.ImageView;
 
 import com.igordubrovin.tfsmsg.R;
 
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Игорь on 18.04.2017.
  */
 
 public class ImageAdapter extends BaseAdapter {
     private Context mContext;
+
+    private List<Thread> threadList = new ArrayList<>();
 
     // Constructor
     public ImageAdapter(Context c) {
@@ -51,27 +58,84 @@ public class ImageAdapter extends BaseAdapter {
             int height = size.y/3;
             imageView = new ImageView(mContext);
             imageView.setLayoutParams(new GridView.LayoutParams(width, height));
-            imageView.setBackgroundColor(Color.RED);
             imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
             imageView.setImageResource(mThumbIds[position]);
-            AnimatedVectorDrawable avd;
+            switch (position){
+                case 0: imageView.setBackgroundColor(ContextCompat.getColor(mContext, R.color.color_background_first_image));
+                    break;
+                case 1: imageView.setBackgroundColor(ContextCompat.getColor(mContext, R.color.color_background_second_image));
+                    break;
+                case 2: imageView.setBackgroundColor(ContextCompat.getColor(mContext, R.color.color_background_third_image));
+                    break;
+                case 3: imageView.setBackgroundColor(ContextCompat.getColor(mContext, R.color.color_background_fourth_image));
+                    break;
+                case 4: imageView.setBackgroundColor(ContextCompat.getColor(mContext, R.color.color_background_fifth_image));
+                    break;
+                case 5: imageView.setBackgroundColor(ContextCompat.getColor(mContext, R.color.color_background_sixth_image));
+                    break;
+                default: imageView.setBackgroundColor(ContextCompat.getColor(mContext, R.color.color_background_default_image));
+                    break;
+            }
+
+            final AnimatedVectorDrawable avd;
             Drawable drawable = imageView.getDrawable();
             avd = (AnimatedVectorDrawable) drawable;
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 avd.start();
             }
+
+            final Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (true) {
+                            Thread.sleep(1500);
+                            if (Thread.currentThread().isInterrupted())
+                                break;
+                            imageView.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                                        avd.start();
+                                    }
+                                }
+                            });
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            thread.start();
+            threadList.add(thread);
+            /*imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        avd.start();
+                    }
+                }
+            });*/
+
         } else {
             imageView = (ImageView) convertView;
         }
         return imageView;
     }
 
+    public void interruptThreads(){
+        for (Thread t : threadList){
+            t.interrupt();
+        }
+    }
+
     // Keep all Images in array
     public Integer[] mThumbIds = {
             //here you can place the image
-           /* R.drawable.avd_vector_anim_cancel, R.drawable.avd_vector_anim_emoji,
+            R.drawable.avd_vector_anim_cancel, R.drawable.avd_vector_anim_emoji,
             R.drawable.avd_vector_anim_keyboard, R.drawable.avd_vector_anim_send,
-            R.drawable.avd_vector_mess, R.drawable.avd_vector_anim_chat*/
-            R.drawable.avd_vector_anim_cancel
+            R.drawable.avd_vector_mess, R.drawable.avd_vector_anim_chat
     };
+
 }
